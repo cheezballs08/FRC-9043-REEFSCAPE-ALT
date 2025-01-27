37a6cb59c38ff82910d2617120569a82f6ed741c
cedar <gamerulzproz@gmail.com>
2025-01-27 17:08:07 +0300

    Kod refaktör edildi ve açıklamalar türçeye çevirildi (türkçe yazıyoz açıklamaları)

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -11,15 +7,14 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeIntakeSubsystem extends SubsystemBase {
-    SparkMax algaeIntakeMotor1, algaeIntakeMotor2;
+  
+  SparkMax algaeIntakeMotor1, algaeIntakeMotor2;
 
-    //Constants
-    int algaeIntakeMotor1ID = 0, algaeIntakeMotor2ID = 0;
-    MotorType algaeIntakeMotor1Type = null, algaeIntakeMotor2Type = null;
-    boolean algaeIntakeMotor1Inv = false, algaeIntakeMotor2Inv = false;
-    SparkBaseConfig sparkBaseConfig;
-
-
+  // Sabitler
+  int algaeIntakeMotor1ID = 0, algaeIntakeMotor2ID = 0;
+  MotorType algaeIntakeMotor1Type = null, algaeIntakeMotor2Type = null;
+  boolean algaeIntakeMotor1Inv = false, algaeIntakeMotor2Inv = false;
+  SparkBaseConfig sparkBaseConfig;
 
   public AlgaeIntakeSubsystem() {
     this.algaeIntakeMotor1 = new SparkMax(algaeIntakeMotor1ID, algaeIntakeMotor1Type);
@@ -27,6 +22,9 @@ public class AlgaeIntakeSubsystem extends SubsystemBase {
     this.algaeIntakeMotor2 = new SparkMax(algaeIntakeMotor2ID, algaeIntakeMotor2Type);
     this.algaeIntakeMotor2.configure(sparkBaseConfig.inverted(algaeIntakeMotor2Inv), null, null);
   }
+
+  @Override
+  public void periodic() {}
 
   public void SetAlgaeIntakeMotorsSpeeds(double speed){
     algaeIntakeMotor1.set(speed);
@@ -38,8 +36,4 @@ public class AlgaeIntakeSubsystem extends SubsystemBase {
     SetAlgaeIntakeMotorsSpeeds(speed);
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
 }
